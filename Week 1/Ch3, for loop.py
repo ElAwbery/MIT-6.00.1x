0b464e703668e46196d864f1deb57d5be2cbf3af
ElAwbery <36336917+ElAwbery@users.noreply.github.com>
2018-06-18 14:48:49 +0100

    Update Ch3, for loop.py

--- Week 1/Ch3, for loop.py
+++ Week 1/Ch3, for loop.py
@@ -3,7 +3,7 @@
 """
 Created on Mon Mar  5 14:07:41 2018
 
-@author: Charlie
+@author: ElAwbery
 """
 
 # Let s be a string that contains a sequence of decimal numbers separated by commas
@@ -15,22 +15,19 @@ total = 0
 for num in ('1.23','2.4','4'):  # took a while to figure out the syntax that would work. Each float has to be a separate list literal. 
     total = total + float(num)
 print (total)
-'''
-'''
+
+#practice code follows
 total = 0
 
 for num in range (1, 7):
     total = total + num
 print (total)
    
-''' 
-'''
 num = 5
 if num > 2:
     print(num)
     num -= 1
 print(num)
-
 
 num = 5
 for num in range (0, 3):

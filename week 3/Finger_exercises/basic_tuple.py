4f828bc9a87704e4feef3d4795c89dff40175f50
ElAwbery <36336917+ElAwbery@users.noreply.github.com>
2018-06-19 17:18:12 +0100

    Update and rename week 3/wk3oddTuple1.py to week 3/Finger_exercises/basic_tuple.py

--- week 3/Finger_exercises/basic_tuple.py
+++ week 3/Finger_exercises/basic_tuple.py
@@ -5,15 +5,11 @@ Spyder Editor
 This is a temporary script file.
 """
 
-
-
 def oddTuples(aTup):
     '''
     aTup: a tuple
-    
     returns: tuple, every other element of aTup. 
     '''
-
     count = 0
     newTup = ()
 

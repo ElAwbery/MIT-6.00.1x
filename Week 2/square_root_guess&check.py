23ae928b6caafd5007af06896b316af98dee0826
ElAwbery <36336917+ElAwbery@users.noreply.github.com>
2018-06-19 16:30:46 +0100

    Update square_root_guess&check.py

--- Week 2/square_root_guess&check.py
+++ Week 2/square_root_guess&check.py
@@ -4,17 +4,14 @@ Spyder Editor
 
 This is a temporary script file.
 """
- # In the square root program, if the square root of a value between 0 and 1 is sought, the 
- # incremental program written to find the square root of an integer greater than 1 will fail. 
- # This is because if the answer starts at 0 and increments, it will always be greater than 0. 
- 
-    # the condition that stops the program running is that the answer variable exceeds the size of x before it gets near the 
-    # value of the correct square root, because the square root value in this instance is larger than the answer
+'''
+If the square root of a value between 0 and 1 is sought, an incremental program written to find the square root of an integer 
+greater than 1 will fail. 
+
+This is because the answer starts at 0 and increments, it will always be greater than 0. 
     
- # The program can be rewritten to decrement, and then it will work. As written here: 
-
-
-
+The program can be rewritten to decrement, and then it will work. As below: 
+'''
 
 x = 0.25
 epsilon = 0.01
@@ -32,27 +29,5 @@ else:
     print (ans, "is close to square root of", x)
     
    
-# But the point to be understood is that the test for the answer has to include the range of values near the answer
-# Obviously, if the window for the answer is not reached before the loop terminates, then it won't work
-    
-    
+# The key point to understand is that the test for the answer has to include the range of values near the answer
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
